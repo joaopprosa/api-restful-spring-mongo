e50ba9a52382b76d33eb20c869e7bd9cff4a4b1b
joao rosa <joao.rosa@gec.inatel.br>
2022-03-07 15:00:05 -0300

    Referenciando os posts do usuário

--- src/main/java/com/mongospring/config/Instantiation.java
+++ src/main/java/com/mongospring/config/Instantiation.java
@@ -43,6 +43,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		maria.getPost().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 	
 }
